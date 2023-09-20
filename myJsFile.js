4d44f087e208aa9a581b378d5a33c2d0d6cd00e1
ismaildev49 <ismaelbentatou@gmail.com>
2023-09-20 16:04:47 +0200

    fin

---
+++ myJsFile.js
@@ -0,0 +1,48 @@
+let maxNumber = prompt("Ecrivez un chiffre maximal SVP.")
+maxNumber = parseInt(maxNumber)
+
+do {
+switch (Number.isNaN(maxNumber)) {
+    case true :
+        maxNumber= prompt("Ceci n'est pas un chiffre. Merci d'entrer un chiffre correct")
+        maxNumber= parseInt(maxNumber)
+        console.log(typeof randomNumber)
+        break
+
+    case false :
+        let randomNumber = Math.floor(Math.random() * (maxNumber+1)) 
+        
+        count = 0
+        attempt = prompt(`Devine le juste prix compris entre 0 et ${maxNumber}`)
+        attempt = parseInt(attempt)
+        do {
+            switch (Number.isNaN(attempt)) {
+                case false :
+
+                    if (attempt > randomNumber) {
+                        attempt = prompt("C'est moins. Réessaye.")
+                        attempt = parseInt(attempt)
+                        count++
+                        break
+                    } else if (attempt < randomNumber) {
+                        attempt= prompt("C'est plus. Réessaye")
+                        attempt = parseInt(attempt)
+                        count++
+                        break
+                    }
+                case true :
+                    attempt= prompt("Ceci n'est pas un chiffre. Merci d'entrer un chiffre correct")
+                    attempt = parseInt(attempt)
+                    count++
+                    break
+    }
+
+        } while (attempt != randomNumber)
+count++
+alert(`Félicitations ! Tu as correctement deviné le juste prix en ${count} essais !`)
+
+    
+}
+} while( Number.isNaN(maxNumber) == true)
+
+
