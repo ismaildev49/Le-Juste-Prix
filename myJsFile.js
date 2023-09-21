d4631a0acb27f2b52603c982d2a2524b841fbd79
ismaildev49 <ismaelbentatou@gmail.com>
2023-09-21 11:38:53 +0200

    fin corrigé

--- myJsFile.js
+++ myJsFile.js
@@ -1,48 +1,61 @@
-let maxNumber = prompt("Ecrivez un chiffre maximal SVP.")
-maxNumber = parseInt(maxNumber)
+let maxNumber
+let isValid = false
+let count = 0
 
-do {
-switch (Number.isInteger(maxNumber)) {
-    case true :
-        maxNumber= prompt("Ceci n'est pas un chiffre. Merci d'entrer un chiffre correct")
-        maxNumber= parseInt(maxNumber)
-        console.log(typeof randomNumber)
-        break
+while (!isValid) {
+  maxNumber = prompt("Choisissez un nombre maximal")
 
-    case false :
-        let randomNumber = Math.floor(Math.random() * (maxNumber+1)) 
-        
-        count = 0
-        attempt = prompt(`Devine le juste prix compris entre 0 et ${maxNumber}`)
-        attempt = parseInt(attempt)
-        do {
-            switch (Number.isInteger(attempt)) {
-                case false :
 
-                    if (attempt > randomNumber) {
-                        attempt = prompt("C'est moins. Réessaye.")
-                        attempt = parseInt(attempt)
-                        count++
-                        break
-                    } else if (attempt < randomNumber) {
-                        attempt= prompt("C'est plus. Réessaye")
-                        attempt = parseInt(attempt)
-                        count++
-                        break
-                    }
-                case true :
-                    attempt= prompt("Ceci n'est pas un chiffre. Merci d'entrer un chiffre correct")
-                    attempt = parseInt(attempt)
-                    count++
-                    break
+  if (maxNumber!=null && maxNumber!= "") {
+
+
+    if (!isNaN(maxNumber)) {
+
+      maxNumber = parseInt(maxNumber)
+      if (maxNumber>0) {
+        isValid = true
+      } else{
+        alert ("Entrez un nombre plus grand que zéro")
+      }
+    } else {
+      alert("Ceci n'est pas un nombre")
     }
-
-        } while (attempt != randomNumber)
-count++
-alert(`Félicitations ! Tu as correctement deviné le juste prix en ${count} essais !`)
-
-    
+  } else { 
+    alert("Ceci n'est pas un nombre")
+  }
 }
-} while( Number.isNaN(maxNumber) == true)
 
+let randomNumber = Math.floor(Math.random()*maxNumber) +1
+
+let attempt
+
+isValid = false
+
+while ( !isValid) {
+  attempt = prompt( `Devinez le nombre entre 0 et ${maxNumber}`)
+  if (attempt != null && attempt != ""){
+    if (!isNaN(attempt)) {
+      isValid=true
+      attempt = parseInt(attempt)
+      count ++
+      if(attempt==randomNumber) {
+        alert(`Félicitations ! Tu as trouvé le juste prix en ${count} essais`)
+        isValid=true
+      } else if (attempt< randomNumber) {
+        alert("C'est plus. Réessayez")
+        isValid = false
+        continue
+      } else {
+        alert( "Cest moins. Réessayez")
+        isValid=false
+        continue
+      }
+    } else {
+      alert("Ceci n'est pas un nombre")
+    }
+  } else {
+    alert("Ceci n'est pas un nombre")
+  }
+
+}
 
